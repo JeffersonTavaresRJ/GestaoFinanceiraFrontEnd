2ab71a0d6701ed423680115eb5d801bab3199c7c
JeffersonTavaresRJ <34421064+JeffersonTavaresRJ@users.noreply.github.com>
2021-03-09 21:57:38 -0300

    09-03-2021:  Conexão Local
    1) Alteração apiUrl para apontar para api  localhost da aplicação Net. Core 3

--- appGestaoFinanceira/src/environments/environment.ts
+++ appGestaoFinanceira/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl:'http://gestaofinanceiraapi/'
+ // apiUrl:'http://gestaofinanceiraapi/'
+    apiUrl: 'https://localhost:44381/'
 };
 
 /*
