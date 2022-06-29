cea685c3b17c8cb95d186733f745b234f77f4be1
Jefferson Tavares <jef.tavaresrj@gmail.com>
2022-06-29 18:06:49 -0300

    29-06-2022: Alteração layout Movimentação Realizada

--- appGestaoFinanceira/src/app/features/lancamentos/mov-realizada/mov-realizada-list/mov-realizada-list.component.ts
+++ appGestaoFinanceira/src/app/features/lancamentos/mov-realizada/mov-realizada-list/mov-realizada-list.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute } from '@angular/router';
 import { Conta } from 'src/app/features/cadastros-basicos/_models/conta-model';
 import { AlertMessageForm } from 'src/app/shared/components/alert-form/alert-message-form';
 import { DateConvert } from 'src/app/shared/functions/date-convert';
-import { MovimentacaoRealizada } from '../../_models/mov-realizada-model.';
 import { MovRealizadaService } from '../../_services/mov-realizada-service';
 
 @Component({
@@ -23,8 +22,6 @@ export class MovRealizadaListComponent implements OnInit {
   dataIni: Date;
   dataFim: Date;
   idMovimentacaoRealizada: number;
-  valorTotalReceita: number = 0;
-  valorTotalDespesa: number = 0;
 
   constructor(private actResourceRoute: ActivatedRoute,
     private movRealizadaService: MovRealizadaService,
@@ -59,45 +56,42 @@ export class MovRealizadaListComponent implements OnInit {
   }
 
   filtrarTablePorPeriodo() {
-    this.dataFim = new Date(this.dataFim.getFullYear(),
-      this.dataFim.getMonth() + 1,
+    debugger;
+    this.dataFim = new Date(this.dataIni.getFullYear(),
+      this.dataIni.getMonth() + 1,
       0);
-
-    if (this.dataFim < this.dataIni) {
-      this.alertMessageForm.showError("O período informado possui o mês/ano inicial maior do que o mês/ano final", "Sr. Usuário");
-      return false;
-    }
 
     this.movRealizadaService.GetGroupBySaldoDiario(DateConvert.formatDateYYYYMMDD(this.dataIni.toString(), '-'),
       DateConvert.formatDateYYYYMMDD(this.dataFim.toString(), '-')).subscribe(
         (sucess: any[]) => {
           this.results = sucess;
           this.resultsAux = this.results;
-          this.calcularSaldo();
         });
   }
 
   getConta(_ev: Conta) {
-    // this.formGroup.get("idConta").setValue(_ev.id);
     this.results = this.resultsAux.filter(x => x.conta.id === this.formGroup.get("idConta").value);
   }
 
   filtrarTablePorParametros(event?: any) {
+    debugger;
     this.results = this.resultsAux;
-    var idCategoria = this.formGroup.get('idCategoria').value;
     var idItemMovimentacao = this.formGroup.get('idItemMovimentacao').value;
     var idConta = this.formGroup.get('idConta').value;
 
-    if (idCategoria != null) {
-      this.results = this.resultsAux.filter(m => m.itemMovimentacao.categoria.id == idCategoria);
-    }
     if (idItemMovimentacao != null) {
-      this.results = this.resultsAux.filter(m => m.itemMovimentacao.id == idItemMovimentacao);
+      this.resultsAux.map(function(item, idx){
+          console.log(item[idx].movimentacoesRealizadas.valor);
+          /*item[idx].movimentacoesRealizadas.foreach(element=>{
+            if(element.itemMovimentacao.id ==idItemMovimentacao){
+              this.results.push(item);
+            }
+          })*/
+      })
     }
     if (idConta != null) {
       this.results = this.resultsAux.filter(m => m.conta.id == idConta);
     }
-    this.calcularSaldo();
   }
 
   modalDeleteMessage(_idMovimentacaoRealizada: number) {
@@ -113,35 +107,4 @@ export class MovRealizadaListComponent implements OnInit {
         });
     }
   }
-
-  private calcularSaldo() {
-    this.valorTotalDespesa = 0;
-    this.valorTotalReceita = 0;
-    this.results.forEach((element: MovimentacaoRealizada) => {
-      if (element.itemMovimentacao.tipo == "D") {
-        this.valorTotalDespesa += element.valor;
-      } else {
-        this.valorTotalReceita += element.valor;
-      }
-    });
-  }
-  /*
-  GroupByDataMovimentacaoRealizada(arMovRealizada: MovimentacaoRealizada[]): any[]
-  {
-    debugger;
-    arMovRealizada.sort(function(a,b){return (((new Date(b.dataMovimentacaoRealizada).getFullYear()*10000)+(new Date(b.dataMovimentacaoRealizada).getMonth()*100)+(new Date(b.dataMovimentacaoRealizada).getDate()))-
-                                              ((new Date(a.dataMovimentacaoRealizada).getFullYear()*10000)+(new Date(a.dataMovimentacaoRealizada).getMonth()*100)+(new Date(a.dataMovimentacaoRealizada).getDate())));
-  }); 
-    var groups = new Set(arMovRealizada.map(item => item.dataMovimentacaoRealizada)), 
-    resultGroup = [];
-    groups.forEach(g => 
-      resultGroup.push(
-              {
-                dataMovimentacaoRealizada: g, 
-                values: arMovRealizada.filter(i => i.dataMovimentacaoRealizada === g)
-              })
-           );
-    return resultGroup;
-  }
-  */
 }
