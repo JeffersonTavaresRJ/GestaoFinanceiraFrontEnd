e52add2f3a79a7968f72d0f53c4b44d768ee78ac
JeffersonTavaresRJ <jef.tavaresrj@gmail.com>
2022-10-08 22:15:49 -0300

    08-10-2022: Ajuste no parâmetro conta do Fluxo Diário

--- appGestaoFinanceira/src/app/features/cadastros-basicos/_services/commands/conta/conta-cmd-update.ts
+++ appGestaoFinanceira/src/app/features/cadastros-basicos/_services/commands/conta/conta-cmd-update.ts
@@ -1,18 +1,29 @@
 import { FormGroup } from "@angular/forms";
 import { GenericCommand } from "src/app/shared/_services/commands/generic-cmd";
+import { Conta } from "../../../_models/conta-model";
 
 export class ContaCommandUpdate extends GenericCommand {
     constructor(
         public id: number=null,
         public descricao: string = null,
-        public status: boolean = null
+        public status: boolean = null,
+        public defaultConta: string = 'N'
     ) { super(); };
 
     static convertFormGroupToCommand(formGroup: FormGroup):ContaCommandUpdate{
         return new ContaCommandUpdate(
             formGroup.get('id').value,
             formGroup.get('descricao').value,
-            formGroup.get('status').value
+            formGroup.get('status').value,
+            formGroup.get('defaultConta').value
+        )
+    }
+    static convertModelToCommand(conta: Conta):ContaCommandUpdate{
+        return new ContaCommandUpdate(
+            conta.id,
+            conta.descricao,
+            conta.status,
+            conta.defaultConta
         )
     }
 }
--- appGestaoFinanceira/src/app/features/lancamentos/mov-realizada/mov-realizada-list/mov-realizada-list.component.ts
+++ appGestaoFinanceira/src/app/features/lancamentos/mov-realizada/mov-realizada-list/mov-realizada-list.component.ts
@@ -1,7 +1,10 @@
+import { ThrowStmt } from '@angular/compiler';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Conta } from 'src/app/features/cadastros-basicos/_models/conta-model';
+import { ContaService } from 'src/app/features/cadastros-basicos/_services/conta-service';
 import { AlertMessageForm } from 'src/app/shared/components/alert-form/alert-message-form';
 import { DateConvert } from 'src/app/shared/functions/date-convert';
 import { MovimentacaoRealizada } from '../../_models/mov-realizada-model.';
@@ -25,14 +28,16 @@ export class MovRealizadaListComponent implements OnInit {
   displayDetalhe:boolean;
   idMovimentacaoRealizada: number;
   movimentacaoRealizadaDetalhe: MovimentacaoRealizada = new MovimentacaoRealizada();
+  contaOld: Conta;
 
 
   constructor(private actResourceRoute: ActivatedRoute,
     private movRealizadaService: MovRealizadaService,
+    private contaService : ContaService,
     private alertMessageForm: AlertMessageForm,
-    protected formBuilder: FormBuilder) {
+    protected formBuilder: FormBuilder) {    
     this.formGroup = this.formBuilder.group({
-      idConta: [1],
+      idConta: [null],
     });
   }
 
@@ -54,6 +59,14 @@ export class MovRealizadaListComponent implements OnInit {
       (sucess: { resolveMovReal: any[] }) => {
         this.results = sucess.resolveMovReal;
         this.resultsAux = this.results;
+
+        //Setando a conta default como parâmetro de pesquisa..
+        this.contaService.getAll().subscribe(
+          sucess=>{
+            this.formGroup.get('idConta').setValue(sucess.filter(x=>x.defaultConta=="S")[0].id);
+          }
+        )
+        
       }
     );
   }
@@ -73,17 +86,34 @@ export class MovRealizadaListComponent implements OnInit {
         });
   }
 
-  getConta(_ev: Conta) {
-    this.results = this.resultsAux.filter(x => x.conta.id === this.formGroup.get("idConta").value);
-  }
-
-  filtrarTablePorParametros(event?: any) {
+  filtrarTablePorParametros(conta?: Conta) {
     debugger;
     this.results = this.resultsAux;
-    var idConta = this.formGroup.get('idConta').value;
 
-    if (idConta != null) {
-      this.results = this.resultsAux.filter(m => m.conta.id == idConta);
+    if (conta != null){
+
+      //alterando a conta anterior que foi utilizada como filtro para deixar de ser o filtro default para pesquisa..
+      if(this.contaOld != null){
+        this.contaOld.defaultConta = 'N';
+        this.contaService.putConta(this.contaOld).subscribe(
+          sucess=>{
+            this.contaOld = conta;
+          }
+        )
+      }    
+  
+      //alterando a conta atual que está sendo utilizada como filtro para ser o filtro default para pesquisa..
+      conta.defaultConta = 'S';
+      this.contaService.putConta(conta).subscribe(
+        sucess=>{
+          this.contaOld = conta;
+        }
+      )
+
+      this.results = this.resultsAux.filter(m => m.conta.id == conta.id);
+
+    }else{
+      this.alertMessageForm.showError("A conta deve ser informada", "Sr. Usuário");
     }
   }
 
