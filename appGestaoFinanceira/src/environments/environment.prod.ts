e7f34f6466d34a450817fa64131ce50ea4cda53f
JeffersonTavaresRJ <jef.tavaresrj@gmail.com>
2024-08-21 10:21:41 -0300

    21-08-2024: Acerto environment prod

--- appGestaoFinanceira/src/environments/environment.prod.ts
+++ appGestaoFinanceira/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   initRouter:'/receitas-despesas-dashboard',
   IdxConnection:0,  
   arApiUrl: ['http://gestaofinanceiraapi/'],
+  credentials:[],
   keyUser:'usuario',
   keyParamListMovPre: 'ParamListMovPre'
 };
