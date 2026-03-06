b3b470859affa3604e92e52121ae533e10d8c27f
JeffersonTavaresRJ <jef.tavaresrj@gmail.com>
2026-03-06 18:00:15 -0300

    06-03-2026: Correção na conversão da data
    Testes em Movimentação Prevista e Realizada

--- appGestaoFinanceira/src/app/shared/functions/date-convert.ts
+++ appGestaoFinanceira/src/app/shared/functions/date-convert.ts
@@ -2,7 +2,7 @@ export class DateConvert{
 
     static formatDateDDMMYYYY(date, separador):string {
         date = date + 'T10:11:08.000Z';
-        var d = new Date(date),
+        var d = DateConvert.convertDate(date, separador),
             month = '' + (d.getMonth() + 1),
             day = '' + d.getDate(),
             year = d.getFullYear();
@@ -18,7 +18,7 @@ export class DateConvert{
 
     static formatDateYYYYMMDD(dateTime, separador):string {
         //dateTime = dateTime + 'T10:11:08.000Z';
-        var d = DateConvert.stringToDate(dateTime, separador),
+        var d = DateConvert.convertDate(dateTime, separador),
             month = '' + (d.getMonth() + 1),
             day = '' + d.getDate(),
             year = d.getFullYear();
@@ -33,9 +33,9 @@ export class DateConvert{
 
     static formatDateMMYYYY(dateTime, separador):string {
         //dateTime = dateTime + 'T10:11:08.000Z';
-        var d = DateConvert.stringToDate(dateTime, separador),
-            month = '' + (d.getMonth() + 1),
-            year = d.getFullYear();
+        var date=DateConvert.convertDate(dateTime, separador),
+            month = '' + (date.getMonth() + 1),
+            year  = date.getFullYear();
     
         if (month.length < 2) 
             month = '0' + month;
@@ -44,7 +44,8 @@ export class DateConvert{
     }
 
     static stringToDate(dateTime:string, separador:string):Date{  
-      //date = date + 'T10:11:08.000Z';  
+      //date = date + 'T10:11:08.000Z'; 
+      debugger; 
         var date = dateTime.split("T")[0];//desconsiderando o time da data
 
         var arrData = date.split(separador);
@@ -53,6 +54,20 @@ export class DateConvert{
         }
         //console.log('montagem data: '+arrData[0] + '-' + arrData[1] + '-' + arrData[2]);
         return new Date(Number.parseInt(arrData[0]), Number.parseInt(arrData[1])-1, Number.parseInt(arrData[2]));  
-    }   
+    }
+    
+    static isDateValid(value: any): value is Date {
+       return value instanceof Date && !isNaN(value.getTime());
+    }
+
+    static convertDate(dateTime: any, separador: string): Date{
+        var date=null;
+        if (DateConvert.isDateValid(dateTime)){
+            date = new Date(dateTime);
+        }else{
+            date = DateConvert.stringToDate(dateTime, separador);
+        }
+        return date;
+    }
    
 }
